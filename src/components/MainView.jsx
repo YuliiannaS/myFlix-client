7b96ecea41bd22f70247f0f66ade04795e1f0190
Yuliia <yulja01@gmail.com>
2023-09-25 21:36:32 -0500

    added login and signup views

--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -1,18 +1,43 @@
 import { useState, useEffect } from "react";
+import { LoginView } from './LoginView';
+import { SignupView } from './SignupView';
 import { MovieCard } from './MovieCard';
 import { MovieView } from './MovieView';
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [user, setUser] = useState(storedUser);
+  const [token, setToken] = useState(storedToken);
 
   useEffect(() => {
-    fetch("https://fathomless-coast-10170-8a6e0563517f.herokuapp.com/movies")
+    if (!token) {
+      return;
+    }
+
+    fetch("https://fathomless-coast-10170-8a6e0563517f.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` }
+    })
       .then((response) => response.json())
       .then((data) => {
         setMovies(data);
       });
-  }, []);
+  }, [token]);
+
+  if (!user) {
+    return (
+      <>
+        <LoginView onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }} />
+        or
+        <SignupView />
+      </>
+    );
+  }
 
   if (selectedMovie) {
     return <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />;
@@ -23,16 +48,19 @@ export const MainView = () => {
   }
 
   return (
-    <div>
-      {movies.map((movie) => (
-        <MovieCard
-          key={movie._id}
-          movie={movie}
-          onMovieClick={(newSelectedMovie) => {
-            setSelectedMovie(newSelectedMovie);
-          }}
-        />
-      ))}
-    </div>
+    <>
+      <div>
+        {movies.map((movie) => (
+          <MovieCard
+            key={movie._id}
+            movie={movie}
+            onMovieClick={(newSelectedMovie) => {
+              setSelectedMovie(newSelectedMovie);
+            }}
+          />
+        ))}
+      </div>
+      <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
+    </>
   );
 };
